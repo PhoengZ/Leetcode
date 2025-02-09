bea2f2798cd6353e3e91d103c2d83fca565c4afc
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-02-10 06:24:15 +0700

    Update solution combination sum2

--- Combination_sum2.cpp
+++ Combination_sum2.cpp
@@ -1,43 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-//pass 172/176 testcase
-void my_recur(vector<vector<int>>&candidate,vector<int>&st,int target,int total,vector<int>&select,int start){
-    for (int i = start;i<select.size();i++){
-        int e = select[i];
-        if ((st.empty() && i>0 && e == select[i-1] ))continue;
-        if (total+e < target){
-            st.push_back(e);
-            my_recur(candidate,st,target,total+e,select,i+1);
-            st.pop_back();
-        }else if (total + e == target){
-            st.push_back(e);
-            auto low = lower_bound(candidate.begin(),candidate.end(),st);
-            if (candidate.empty() || st != *low)candidate.push_back(st);
-            st.pop_back();
+class Solution {
+    public:
+        vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+            sort(candidates.begin(),candidates.end());
+            vector<vector<int>>v;
+            vector<int> now;
+            f(v,candidates,now,target,0,0);
+            return v;
         }
-    }
-}
-vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-    sort(candidates.begin(),candidates.end());
-    vector<int> st;
-    vector<vector<int>> vp;
-    my_recur(vp,st,target,0,candidates,0);
-    return vp;
-}
-
-int main(){
-    int n,t;
-    cin>>n>>t;
-    vector<int> v(n);
-    for (int i =0;i<n;i++)cin>>v[i];
-    vector <vector<int>> vp = combinationSum2(v,t);
-    for (int i = 0;i<vp.size();i++){
-        cout << "size index: " << vp[i].size()<<endl;
-        for (auto&e:vp[i])cout << e << " ";
-        cout <<endl;
-    }
-    return 0;
-}
+        void f(vector<vector<int>>&v, vector<int>&can, vector<int>&now,int target, int n, int idx){
+            if (n == target){
+                v.push_back(now);
+                return;
+            }
+            if (n > target)return;
+            for (int i = idx;i<can.size();i++){
+                if (i>idx && can[i] == can[i-1])continue;
+                now.push_back(can[i]);
+                f(v,can,now,target,n+can[i],i+1);
+                now.pop_back();
+            }
+        }
+    };
